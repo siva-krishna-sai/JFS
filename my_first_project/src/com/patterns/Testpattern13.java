748c0e174ce45277f892e7432e0ce2ad2f05019e
sivak <sivak@DESKTOP-5UMP5A4>
2026-02-17 21:30:12 +0530

    update

---
+++ my_first_project/src/com/patterns/Testpattern13.java
@@ -0,0 +1,37 @@
+package com.patterns;
+
+public class Testpattern13 {
+
+	public static void main(String[] args) {
+		int n = 6;
+
+       
+        for (int i = 1; i <= n; i++) {
+
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+			System.out.println();
+		}
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+	}
+}
---
+++ my_first_project/src/com/patterns/Testpattern8.java
@@ -0,0 +1,17 @@
+package com.patterns;
+
+public class Testpattern8 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=6;i++) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+	}
+
+}
